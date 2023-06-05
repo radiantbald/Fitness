2069003966b65a0546414382d21759454d40015d
Алмаз <61264586+almazof@users.noreply.github.com>
2023-06-05 22:10:54 +0500

    - lesson 3

--- MyOwnWorkout/MyOwnWorkout/AuthPage/RegPageViewController.swift
+++ MyOwnWorkout/MyOwnWorkout/AuthPage/RegPageViewController.swift
@@ -7,12 +7,84 @@
 
 import UIKit
 
+//protocol TestProtocol: AnyObject {
+//
+//}
+//
+//class Test {
+//    weak var delegate: TestProtocol?
+//}
+
+protocol RegPageViewControllerDelegate: AnyObject {
+    func getRegistration(surname: String, name: String, nick: String, phone: String)
+}
+
 class RegPageViewController: UIViewController {
+    weak var delegate: RegPageViewControllerDelegate?
     
-    @IBAction func backButton(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
-        self.tabBarController?.tabBar.isHidden = false
+//    let test = Test()
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nickTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    //1
+    override func loadView() {
+        super.loadView()
+//        print(#function)
+//        test.delegate = self
     }
+    
+    //2
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Регистрация"
+//        print(#function)
+    }
+    
+    //3
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+//        print(#function)
+    }
+    
+    //4
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+//        print(#function)
+    }
+    
+    //5
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+//        print(#function)
+    }
+    
+    
+    //6
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        print(#function)
+    }
+    
+    //7
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        print(#function)
+    }
+    
+    //8
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+//        print(#function)
+        
+    }
+    
+    //9
+    deinit {
+//        print(#function)
+    }
+    
     
     @IBAction func EntryButton(_ sender: UIButton) {
         
@@ -20,8 +92,33 @@ class RegPageViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: false)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func registrationButton(_ sender: UIButton) {
+        //TODO: если нет какого то поля то распечатать текст что нет таких то данных. Если все ОК то печатать success
         
+        //1
+        let surname = surnameTextField.text ?? "-"
+        let name = nameTextField.text ?? "-"
+        let nick = nickTextField.text ?? "-"
+        let phone = phoneTextField.text ?? "-"
+        
+        //2 передать на главную
+            //2-1 delegate
+        delegate?.getRegistration(surname: surname, name: name, nick: nick, phone: phone)
+        
+        //3 уйти из этого экрана
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+}
+
+//MARK: - Text Field
+extension RegPageViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        print(textField.text)
     }
 }
--- MyOwnWorkout/MyOwnWorkout/MainPage/MainPageViewController.swift
+++ MyOwnWorkout/MyOwnWorkout/MainPage/MainPageViewController.swift
@@ -14,22 +14,33 @@ class MainPageViewController: UIViewController {
     @IBOutlet weak var mainPageAvatar: UIImageView!
     @IBOutlet weak var mainPageHeader: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
     @IBAction func avatarButton(_ sender: UIButton) {
-        
-        if isAuth == false {
+        view.backgroundColor = .white
+        if isAuth {
             
             let vc = storyboard?.instantiateViewController(withIdentifier: "PersonPageViewController") as! PersonPageViewController
             vc.modalPresentationStyle = .overFullScreen
             navigationController?.pushViewController(vc, animated: true)
-            self.tabBarController?.tabBar.isHidden = true
             print("Личный кабинет")
-        } else {
+        }
+        
+        else { //false
             let vc = storyboard?.instantiateViewController(withIdentifier: "RegPageViewController") as! RegPageViewController
             vc.modalPresentationStyle = .overFullScreen
+            vc.delegate = self
             navigationController?.pushViewController(vc, animated: true)
             self.tabBarController?.tabBar.isHidden = true
             print("Страница регистрации")
-            
         }
             
     }
@@ -40,3 +51,9 @@ class MainPageViewController: UIViewController {
     }
 }
 
+
+extension MainPageViewController: RegPageViewControllerDelegate {
+    func getRegistration(surname: String, name: String, nick: String, phone: String) {
+        print(surname, name, nick, phone)
+    }
+}
